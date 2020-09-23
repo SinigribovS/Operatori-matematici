19e17ddd1af133604b111b88a02ba532e10ffb43
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2020-09-23 10:35:18 +0300

    Add files via upload

---
+++ Calcularea celor 7 exercitii.py
@@ -0,0 +1,15 @@
+import math
+#importam biblioteca math
+print("a) ", 6**7 / (2**4 * 3**5) - (2**48 / 2**18 + (3**4)**5 + 6**23 / 6**13) / (2**10 * 3**10 + 2**17 * 2**13 + (3**5)**4))
+#rezolvam cu folosire de operatori matematici simpli
+print("b) ", (4**5 * 2**13) / 8**7 + (27**4 * 9**5) / 3**20)
+#rezolvam cu folosire de operatori matematici simpli
+print("c) ", 5**103 / (5**17 * 5**8)**4 + (324 / 9**2 + 5 * 2**3) + 10**0) 
+#rezolvam cu folosire de operatori matematici simpli
+print("d) ", ((2**5)**10 + 2**15 * 2**35 + 3**80 / 3**19) / (3**60 + (3**30)**2 + (3**3)**20 + 2**70 / 2**19))
+#rezolvam cu folosire de operatori matematici simpli
+print("e) ", 2 * math.sqrt(3 + math.sqrt(5 - math.sqrt(13 + 4 * math.sqrt(3)))))
+#rezolvam cu folosirea operatorilor din biblioteca math
+print("f) ", math.sqrt(13 + 30 * math.sqrt(2 + math.sqrt(9 + 4 * math.sqrt(2)))))
+#rezolvam cu folosirea operatorilor din biblioteca math
+print("g) ", math.sqrt(26 + 6 * math.sqrt(13 - 4 * math.sqrt(8 + 2 * math.sqrt(6 - 2 * math.sqrt(5))))))
